837b511b66ee40e7b1bdcda4a73b0544d04999b9
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-05-30 22:35:12 +0600

    update profile

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const MyProfile = () => {
     const [user] = useAuthState(auth);
-    console.log(user)
+    const updateProfile =()=>{
+        fetch(`https://http://localhost:5000/user/${user.email}`,{
+        method:'PUT',
+        headers:{
+            authorization:`Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+    .then(res=>{
+        if(res.status ===403){
+            toast.error('Failed to make an admin');
+        }
+        return res.json()})
+    .then(data=>{
+        if(data.modifiedCount >0){
+            
+        toast.success(`Successfully made an admin`);
+        }
+    })
+    }
     return (
         <div className='mt-5'>
             <div class="avatar online">
@@ -14,6 +33,7 @@ const MyProfile = () => {
             </div>
             <p className='uppercase text-xl font-bold font-serif text-secondary'>{user.displayName}</p>
             <p className='text-primary'>{user.email}</p>
+            <button onClick={updateProfile} class="btn btn-xs">Update Profile</button>
         </div>
     );
 };
